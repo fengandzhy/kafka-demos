10d4224303e9c38dd6607420f1de138e14509db4
fengandzhy <fengandzhy@gmail.com>
2023-09-16 15:18:45 +1200

    kafka-consumer ConsumerTest

--- kafka/kafka-consumer/src/test/java/org/frank/kafka/consumer/ConsumerTest.java
+++ kafka/kafka-consumer/src/test/java/org/frank/kafka/consumer/ConsumerTest.java
@@ -18,10 +18,16 @@ public class ConsumerTest {
         props.put("bootstrap.servers", "47.242.251.45:9092");
 
         //消费者分组ID，分组内的消费者只能消费该消息一次，不同分组内的消费者可以重复消费该消息
-        props.put("group.id", "xdclass-g1");
+        props.put("group.id", "xdclass-g2");
 
-        //开启自动提交offset
-        props.put("enable.auto.commit", "true");
+        //默认是latest，如果需要从头消费partition消息，需要改为 earliest 且消费者组名变更，才生效
+        props.put("auto.offset.reset","earliest");
+
+//        //开启自动提交offset
+//        props.put("enable.auto.commit", "true");
+
+        // 开启手动提交
+        props.put("enable.auto.commit", "false");
 
         //自动提交offset延迟时间
         props.put("auto.commit.interval.ms", "1000");
@@ -46,7 +52,16 @@ public class ConsumerTest {
             ConsumerRecords<String,String> records = kafkaConsumer.poll(Duration.ofMillis(100));
             for(ConsumerRecord record : records){
                 System.err.printf("topic=%s, offset=%d,key=%s,value=%s %n",record.topic(),record.offset(),record.key(),record.value());
-            } 
+            }
+            if(!records.isEmpty()){
+                kafkaConsumer.commitAsync((offsets, exception) ->{
+                    if(exception == null){
+                        System.out.println("提交成功！" + offsets.toString());
+                    }else{
+                        System.out.println("提交失败！" + offsets.toString());
+                    }
+                });
+            }            
         }        
     }
 }
